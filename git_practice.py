eed4711887da28a077fbb9089213d6e65052f211
Your NameMwenda <mbobua.mugambi@gmail.com>
2023-06-16 20:02:02 +0300

    second commit

--- git_practice.py
+++ git_practice.py
@@ -1,3 +1,11 @@
 # List = [1,2,3,4,5,6]
 # for num in list:
 print ("Hello World")
+
+
+names = ["Mwenda", "Bobby", "Kyle"]
+
+for name in names:
+    print(name)
+    if name != "Kito":
+        print("Kito's name cannot be found")
